d862d405d48bef0b3ffc5bb16d9a6c0678d48299
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-24 20:31:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
 
-      resp.write song.artist
+      resp.write item.price
 
     else
       resp.write("Route not found")
