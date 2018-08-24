5174bad70cb1a9b083f2aaddebd2ee10306334b3
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-24 20:33:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
-      if item 
+      if item != nil
         resp.write item.price
       else
         resp.write "Item not found"
